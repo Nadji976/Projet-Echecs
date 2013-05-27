3ade1f3ba5787385082e45a714c766116ce43bcf
Nadjimi <nchebani@gmail.com>
2013-05-27 12:24:18 +0200

    dernier seance d' isn

--- Echiquier.py
+++ Echiquier.py
@@ -26,6 +26,12 @@ def pos_vers_coord(c, l):
 def modif_echiquier(echiquier, pos_l, pos_c, piece):
     coord_l, coor_c = pos_vers_coord(pos_l, pos_c)
     echiquier[coord_l][coor_c] = piece
+    
+def deplacer(echiquier, pos_l, pos_c, pos2_l, pos2_c):
+    coord_l, coor_c = pos_vers_coord(pos_l, pos_c)
+    piece = echiquier[coord_l][coor_c]  
+    modif_echiquier(echiquier, pos2_l, pos2_c, piece)
+    modif_echiquier(echiquier, pos_l, pos_c, '*')
 
 echiquier = None  # variable globale contenant l’échiquier
 
@@ -72,14 +78,6 @@ def init_echiquier():
 
 init_echiquier()
 
-def pos_vers_coord(c, l):
-    # E -> 4 et 1 -> 7
-    return (7-"12345678".index (l),"ABCDEFGH".index (c))
-
-def modif_echiquier(echiquier, pos_l, pos_c, piece):
-    coord_l, coor_c = pos_vers_coord(pos_l, pos_c)
-    echiquier[coord_l][coor_c] = piece
-
 def to_html(echiquier):
     html = '''<TABLE BORDER="9">
   <CAPTION> Echiquier </CAPTION>'''
@@ -92,12 +90,6 @@ def to_html(echiquier):
     html = html + '''
 </TABLE>  '''
     return html
-
-def deplacer(echiquier, pos_l, pos_c, pos2_l, pos2_c):
-    coord_l, coor_c = pos_vers_coord(pos_l, pos_c)
-    piece = echiquier[coord_l][coor_c]  
-    modif_echiquier(echiquier, pos2_l, pos2_c, piece)
-    modif_echiquier(echiquier, pos_l, pos_c, '*')
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):    
@@ -113,8 +105,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/html")
             self.end_headers()
             file = open("instructions.html", "r")
+            html = ENTETE
             instructions = file.read()
-            self.wfile.write(bytes(instructions, 'UTF-8'))
+            html=html+instructions
+            html=html+PIED
+            self.wfile.write(bytes(html, 'UTF-8'))
         else:
             self.send_response(200)
             self.send_header("Content-type", "text/html")
@@ -129,8 +124,9 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 params = urllib.parse.parse_qs(query_string)
                 deplacer(echiquier ,params['ld'][0], params['cd'][0], params['la'][0],params['ca'][0])
             html = html + to_html(echiquier)
-            html = html + '<a href="raz">Réinitialiser</a>'
-            
+            html = html +  ' <p><a href="raz">Réinitialiser</a></p>'
+            html = html +  ' <p><a href="instructions.html">Instructions</a></p>'
+            html=html+PIED
             self.wfile.write(bytes(html, 'UTF-8'))
 
 s = http.server.HTTPServer(("localhost", 8000), TestHandler)
